aad2a4bbb2b63ef33cfe3bd91e9ad345e0b646ae
Valentin COPPIN <valentin.coppin@ecole2600.com>
2021-10-16 11:43:00 -0400

    add new line at the end of the document

--- readeof.test.c
+++ readeof.test.c
@@ -1,20 +1,23 @@
 #include "tus.h"
 
 
-int tu_readeof_test(char * str, int expected_pos, char * testname)
+int tu_readeof_test(char * str, int expected_pos, int expected_return, char * testname)
 {
     struct parser *p_parser;
     p_parser = malloc(sizeof(struct parser));
     p_parser->content = str;
     p_parser->current_pos = 0;
 
-    readeof(p_parser);
+    int r;
+
+    r = readeof(p_parser);
 
     printf("\n");
-    if(p_parser->current_pos != expected_pos)
+    if(p_parser->current_pos != expected_pos || r != expected_return)
     {
         printf("!!! tu_readeof: error %s\n", testname);
-        printf("expected parser->current_pos = %d but got %d\n", expected_pos, p_parser->current_pos);
+        printf("expected parser->current_pos = %d got %d\n", expected_pos, p_parser->current_pos);
+        printf("expected return %d got %d\n", expected_return, r);
         free(p_parser);
         return 0;
     }
@@ -30,16 +33,16 @@ int tu_readeof()
     int error = 0;
 
     // Test 1
-    error += tu_readeof_test("\0", 1, "test1");
+    error += tu_readeof_test("\0", 0, 1, "test1");
     
     // Test 2
-    error +=tu_readeof_test("1\0", 0, "test2");
+    error +=tu_readeof_test("1\0", 0, 0, "test2");
     
     // Test 3
-    error +=tu_readeof_test("\0\0", 1, "test3");
+    error +=tu_readeof_test("\0\0", 0, 1, "test3");
 
     // Test 4
-    error +=tu_readeof_test("", 1, "test4");
+    error +=tu_readeof_test("", 0, 1, "test4");
     
 
     return error > 0 ? 1 : 0;
